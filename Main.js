8ecc3647ba0d6674cd02940bb4a31ddfef6562f5
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-06-22 02:04:34 -0500

    Avanze de responsividad

--- Main.js
+++ Main.js
@@ -2,33 +2,33 @@
 let idx = 0;
 
 // Obtencion de elementos html
-let contcarusel = document.getElementById('carouselImg');
-let caruselimg = document.querySelectorAll('#carouselImg .contimg')
-let boxes = document.querySelectorAll('.box')
+let contcarusel = document.getElementById("carouselImg");
+let caruselimg = document.querySelectorAll("#carouselImg .contvideo");
+let boxes = document.querySelectorAll(".box");
 
 // Funcion para cambiar imagenes
 let intervalo = setInterval(() => {
-    idx++
-    if(idx > caruselimg.length - 1) {
-        idx = 0
-    } else if(idx < 0) {
-        idx = caruselimg.length - 1
+    idx++;
+    if (idx > caruselimg.length - 1) {
+        idx = 0;
+    } else if (idx < 0) {
+        idx = caruselimg.length - 1;
     }
 
-    contcarusel.style.transform = `translateX(${-idx * 100}vw)`
-},6000); 
+    contcarusel.style.transform = `translateX(${-idx * 100}vw)`;
+}, 7000);
 
 // Funcion que anima y controlas las cajas en la ventana
-window.addEventListener('scroll', () => {
-    const triggerBottom = window.innerHeight / 5 * 4
+window.addEventListener("scroll", () => {
+    const triggerBottom = (window.innerHeight / 5) * 4;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
 
-        if(boxTop < triggerBottom) {
-            box.classList.add('show')
+        if (boxTop < triggerBottom) {
+            box.classList.add("show");
         } else {
-            box.classList.remove('show')
+            box.classList.remove("show");
         }
-    })
-})
+    });
+});
